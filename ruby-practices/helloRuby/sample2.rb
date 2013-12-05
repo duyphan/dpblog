2e2baf47b5cf6ec4b5246f483cc5b4cbb1750bcf
Phan Duy <duy.phan@asnet.com.vn>
2013-12-05 15:41:14 +0700

    add sample ruby

--- ruby-practices/helloRuby/sample2.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby
-a = [ 45, 3, 19, 8 ]
-b = [ 'sam', 'max', 56, 98.9, 3, 10, 'jill' ]
-print (a + b).join(' '), "\n"
-print a[2], " ", b[4], " ", b[-2], "\n"
-print a.sort.join(' '), "\n"
-a << 57 << 9 << 'phil'
-print "A: ", a.join(' '), "\n"
