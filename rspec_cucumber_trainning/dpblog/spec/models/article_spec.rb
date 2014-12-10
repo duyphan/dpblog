3246535d5db9a30306658f65a379303cde954687
Duy Phan <duy.phan@asnet.com.vn>
2014-12-10 16:29:52 +0700

    implement rspec model

--- rspec_cucumber_trainning/dpblog/spec/models/article_spec.rb
+++ rspec_cucumber_trainning/dpblog/spec/models/article_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Article, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+  	FactoryGirl.create(:article).should be_valid
+  end
+  it "is invalid without a title" do 
+  	FactoryGirl.build(:article, title: nil).should_not be_valid
+  end
+  it "returns a article's title as a string" do 
+  	article = FactoryGirl.create(:article, title: "John Doe", text: "Neque dicta enim quasi. Qui corrupti est quisquam. Facere animi quod aut. Qui nulla consequuntur consectetur sapiente.") 
+  	expect(article.title).to eq("John Doe")
+  end
 end
