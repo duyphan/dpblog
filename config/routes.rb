ee4fc7185328d04eb2c1ab431595368f734cecfa
Duy Phan <duy.phan@asnet.com.vn>
2014-12-26 14:37:14 +0700

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :sessions
   resources :articles
   
-  root 'articles#index'
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
