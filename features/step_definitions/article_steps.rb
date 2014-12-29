e656bb3e107ee244870ae3f87a7c3d7668fa8a6e
Duy Phan <duy.phan@asnet.com.vn>
2014-12-29 16:43:17 +0700

    fix homepage

--- features/step_definitions/article_steps.rb
+++ features/step_definitions/article_steps.rb
@@ -5,3 +5,27 @@ end
 Given /^@(\w+) exists and is logged in$/ do |email|
   step(%(@#{email} exists))
 end
+
+When(/^I follow "(.*?)"$/) do |btn|
+  click_link btn
+end
+
+Then(/^I am on the new artile page$/) do
+	visit new_article_path
+end
+
+When(/^I fill in title with "(.*?)"$/) do |arg1|
+  fill_in 'article_title', :with => arg1
+end
+
+When(/^I fill in text with "(.*?)"$/) do |arg1|
+  fill_in 'article_text', :with => arg1
+end
+
+# When(/^I press "(.*?)"$/) do |btn|
+#   click_link btn
+# end
+
+Then(/^I should see "(.*?)" text$/) do |text|
+  page.should have_content(text)
+end
