a35f6228d3d30f98bd845aa89f5e0dcb5408b0c3
Duy Phan <duy.phan@asnet.com.vn>
2014-12-30 10:01:06 +0700

    remove dpblog files

--- app/models/article.rb
+++
@@ -1,9 +0,0 @@
-class Article < ActiveRecord::Base
-	validates :title, presence: true
-
-	belongs_to :user
-
-	def self.by_letter(letter) 
-		where("title LIKE ?", "#{letter}%").order(:title) 
-	end
-end
